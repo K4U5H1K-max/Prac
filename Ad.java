b8da88042ea8ab9885e6979934c5c772f3e53518
K4U5H1K-max <ponnaboinakaushik@gmail.com>
2026-06-27 14:25:13 +0530

    Declare n1 and n2 as local variables

--- Ad.java
+++ Ad.java
@@ -1,7 +1,7 @@
 class Ad{
   public static void main(String args[]){
-    n1 = Integer.parseInt(args[0]);
-    n2 = Integer.parseInt(args[1]);
+    int n1 = Integer.parseInt(args[0]);
+    int n2 = Integer.parseInt(args[1]);
 
     System.out.println("SUM: "+(n1+n2));
   }
